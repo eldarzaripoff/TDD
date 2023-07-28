042b770a98805235ea2d84450b3dd1227ebf20bc
eldar <eldar.zaripoff@gmail.com>
2023-07-28 23:15:50 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,9 @@
 package org.example;
 
+import java.util.Map;
+
 public class PhoneBook {
+    public int add(String name, String number, Map<String, String> contacts) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,31 @@
+import org.example.PhoneBook;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBookTest {
+    @Test
+    public void AddTest() {
+        //Arrange
+        String name = "Archi";
+        String number = "+79322154695";
+        Map<String, String> map = new HashMap<>();
+        PhoneBook phoneBookTest = Mockito.mock(PhoneBook.class);
+
+        //Act
+        int expercted = phoneBookTest.add(name, number, map);
+        if (!map.containsKey(name)) {
+            map.put(name, number);
+        }
+        int result = map.size();
+
+        //Assert
+        Assertions.assertEquals(expercted, result);
+
+
+
+    }
 }
